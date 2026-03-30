6e377fbf3fc01b0324805c32bf1b8006dee6c056
bichon <imane.bichon@icloud.com>
2026-03-30 13:09:50 +0200

    Add profiles, exercises, progress and game modules

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -4,6 +4,7 @@ import { SupabaseModule } from './supabase/supabase.module';
 import { ProfilesModule } from './profiles/profiles.module';
 import { ExercisesModule } from './exercises/exercises.module';
 import { ProgressModule } from './progress/progress.module';
+import { GameModule } from './game/game.module';
 import configuration from './config/configuration';
 
 @Module({
@@ -16,6 +17,7 @@ import configuration from './config/configuration';
     ProfilesModule,
     ExercisesModule,
     ProgressModule,
+    GameModule,
   ],
 })
 export class AppModule {}
