0238eb627025269774804afaf6d7b380d0b89276
ethanleglise <ethanleglise33@gmail.com>
2026-03-31 14:50:59 +0200

    fix: allow frontend render URL in backend CORS

--- server/src/main.ts
+++ server/src/main.ts
@@ -4,8 +4,14 @@ import { AppModule } from './app.module'
 async function bootstrap() {
   const app = await NestFactory.create(AppModule)
 
+  const corsOrigins = [
+    'http://localhost:5173',
+    'http://127.0.0.1:5173',
+    process.env.FRONTEND_URL,
+  ].filter(Boolean)
+
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: corsOrigins,
     methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
     credentials: true,
   })
